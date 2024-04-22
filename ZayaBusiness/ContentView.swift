60fc94fb97a09905e24f31938a9c5a8db67db548
Fresnel <fresnelk30@icloud.com>
2024-04-22 08:52:33 +0100

    Initial Commit

---
+++ ZayaBusiness/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ZayaBusiness
+//
+//  Created by SwiftCraft on 4/22/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ZayaBusiness/ZayaBusinessApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZayaBusinessApp.swift
+//  ZayaBusiness
+//
+//  Created by SwiftCraft on 4/22/24.
+//
+
+import SwiftUI
+
+@main
+struct ZayaBusinessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
